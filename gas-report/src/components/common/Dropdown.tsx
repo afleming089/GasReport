9e1ad3e467f8e1986734c0c22450c6afa9ac4f32
Anton Fleming <aflemingrocks089@gmail.com>
2026-07-31 21:35:02 -0500

    Started working on a dropdown componenet

--- gas-report/src/components/common/Dropdown.tsx
+++ gas-report/src/components/common/Dropdown.tsx
@@ -1,51 +1,30 @@
-// import { View } from "react-native";
-// import { Text } from "./Text";
-// import { tv, VariantProps } from "tailwind-variants";
+import { View } from "react-native";
+import { Text } from "./Text";
+import { tv, VariantProps } from "tailwind-variants";
 
-// // needs to link or update context
+const dropdown = tv({
+  slots: { base: "" },
 
-// const dropDown = tv({
-//   slots: {
-//     wrapper: "border border-solid border-black rounded-lg p-5 gap-2",
-//     headerGroup: "flex-1 justify-center",
-//     childrenStyles: "",
-//   },
+  variants: {},
+  defaultVariants: {},
+});
 
-//   variants: {
-//     align: {
-//       centered: {
-//         headerGroup: "items-center",
-//       },
-//       left: {
-//         headerGroup: "items-left",
-//       },
-//     },
-//     paddingChildren: {
-//       padding2: { childrenStyles: "p-2" },
-//       paddingNone: { childrenStyles: "p-0" },
-//     },
-//   },
-//   defaultVariants: {
-//     align: "left",
-//     paddingChildren: "padding2",
-//   },
-// });
+type DropdownVariants = VariantProps<typeof dropdown>;
 
-// type CardVariants = VariantProps<typeof card>;
+// return the prop interface
+interface DropdownProps extends DropdownVariants {
+  title: string;
+  children?: React.ReactNode;
+}
 
-// // return the prop interface
-// interface CardProps extends CardVariants {
-//   title: string;
-//   subTitle?: string | number;
-//   children?: React.ReactNode;
-// }
+function Dropdown({ title, ...DropdownVariants }: DropdownProps) {
+  const { base } = dropdown(DropdownVariants);
 
-// function Dropdown() {
-//   return (
-//     <View>
-//       <Text></Text>
-//     </View>
-//   );
-// }
+  return (
+    <View>
+      <Text></Text>
+    </View>
+  );
+}
 
-// export { Dropdown };
+export { Dropdown };
--- gas-report/src/context/UserContext.ts
+++ gas-report/src/context/UserContext.ts
@@ -3,6 +3,7 @@ import { createContext } from "react";
 
 export const UserContext = createContext<User>({
   email: "example@gmail.com",
+  // not real token
   sessionToken: "awdwaidnawnfanjnawjd2ed2413d",
   settings: { defaultFuelType: "Regular", defaultRegion: "Midwest" },
 });
